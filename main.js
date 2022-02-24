3c2310ef258d26975be43cfc866458abd6f24d85
Jérémie LANGLADE <jeremie.langlade@soprasteria.com>
2022-02-24 09:03:45 +0100

    Pré-calcul des voisins.

--- main.js
+++ main.js
@@ -22,23 +22,16 @@ var nbGeneration = 0;
  * Variable de stockage de l'état du jeu
  */
 var gameState = {
-    cells: [],
-    voisins: []
+    aliveCells: [],
+    nbVoisins: new Array(nbRow),
+    voisins: new Array(nbRow)
 };
 /**
- * Tableau de stockage des éléments jQuery des cellules du tableau (pour éviter les interrogations du DOM)
- */
-var vueTableau = [];
-/**
- * Tableau de stockage du nombre de voisins vivants de chaque cellule
- */
-var nbVoisins = [];
-/**
- * Objet jQuery de l'élément DOM de la table
+ * Objet HTMLElement de l'élément DOM de la table
  */
 var laTable = null;
 /**
- * Objet jQuery de l'élément DOM du compteur de génération
+ * Objet HTMLElement de l'élément DOM du compteur de génération
  */
 var generationContainer = null;
 
@@ -49,29 +42,32 @@ var initGame = function() {
     var ts1 = performance.now(),
         ts2 = null,
         html = '',
-        i = null;
+        i = null,
+        listeToutesCellules = null;
     laTable.innerHTML = '';
-    nbRow = parseInt(document.getElementById('nblignes').value);
-    nbCol = parseInt(document.getElementById('nbcolonnes').value);
-    vueTableau = new Array(nbRow);
-    nbVoisins = new Array(nbRow);
+    nbRow = parseInt(document.getElementById('nblignes').value, 10);
+    nbCol = parseInt(document.getElementById('nbcolonnes').value, 10);
+    gameState.nbVoisins = new Array(nbRow);
+    gameState.voisins = new Array(nbRow);
     for (i = 0; i < nbRow; i++) {
         html += '<tr>';
-        vueTableau[i] = new Array(nbCol);
-        nbVoisins[i] = new Array(nbCol);
-        nbVoisins[i].fill(0);
+        gameState.nbVoisins[i] = new Array(nbCol);
+        gameState.voisins[i] = new Array(nbCol);
+        gameState.nbVoisins[i].fill(0);
         for (var j = 0; j < nbCol; j++) {
             var color = Math.floor(Math.random() * 2);
-            html += '<td class="' + (color != 0 ? 'estvivante':'') + '" id="' + i + '-' + j + '" data-row="' + i + '" data-col="' + j + '"></td>';
+            html += '<td class="' + (color != 0 ? 'estvivante':'') + '" id="cell' + i + '_' + j + '"></td>';
         }
         html += '</tr>';
     }
     laTable.innerHTML= html;
-    var cells = laTable.getElementsByTagName('td');
-    for (i = 0; i < cells.length; i++) {
-        var maCellule = cells[i];
-        vueTableau[maCellule.dataset.row][maCellule.dataset.col] = maCellule;
-    }
+    gameState.aliveCells = laTable.querySelectorAll('.estvivante');
+    var tmp = laTable.getElementsByClassName('estvivante');
+    // Calcul des voisins à l'init
+    listeToutesCellules = laTable.getElementsByTagName('td');
+    [].forEach.call(listeToutesCellules, function(maCellule) {
+        gameState.voisins[maCellule.parentElement.rowIndex][maCellule.cellIndex] = getVoisins(maCellule);
+    });
     nbGeneration = 0;
     generationContainer.value = nbGeneration;
     ts2 = performance.now();
@@ -81,13 +77,14 @@ var initGame = function() {
 /**
  * Récupère la liste des cellules voisines d'une case
  * 
- * @param {int} row Numéro de ligne de la cellule
- * @param {int} col Numéro de colonne de la cellule
- * @returns {array<jQuery>} Liste des objets jQuery des cellules adjacentes
+ * @param {HTMLElement} cellule Cellule sont il faut récupérer les voisins
+ * @returns {array<HTMLElement>} Liste des objets des cellules adjacentes
  */
-var getVoisins = function(row, col) {
+var getVoisins = function(cellule) {
     // fonction pour obtenir les voisins de la cellule
     var valeurDeRetour = [],
+        row = cellule.parentElement.rowIndex,
+        col = cellule.cellIndex,
         lignePrecedente = row - 1,
         ligneSuivante = row + 1,
         colonnePrecedente = col - 1,
@@ -95,28 +92,29 @@ var getVoisins = function(row, col) {
     // Traitement de la ligne précédente
     if (lignePrecedente >= 0) {
         if (colonnePrecedente >= 0) {
-            valeurDeRetour.push(vueTableau[lignePrecedente][colonnePrecedente]);
+            valeurDeRetour.push(laTable.rows[lignePrecedente].cells[colonnePrecedente]);
         }
-        valeurDeRetour.push(vueTableau[lignePrecedente][col]);
+        valeurDeRetour.push(laTable.rows[lignePrecedente].cells[col]);
         if (colonneSuivante < nbCol) {
-            valeurDeRetour.push(vueTableau[lignePrecedente][colonneSuivante]);
+            valeurDeRetour.push(laTable.rows[lignePrecedente].cells[colonneSuivante]);
         }
     }
     // Traitement de la ligne en cours
     if (colonnePrecedente >= 0) {
-        valeurDeRetour.push(vueTableau[row][colonnePrecedente]);
+        valeurDeRetour.push(laTable.rows[row].cells[colonnePrecedente]);
     }
+    // On n'ajoute pas la cellule en cours...
     if (colonneSuivante < nbCol) {
-        valeurDeRetour.push(vueTableau[row][colonneSuivante]);
+        valeurDeRetour.push(laTable.rows[row].cells[colonneSuivante]);
     }
-    // Traitement de la ligne précédente
+    // Traitement de la ligne suivante
     if (ligneSuivante < nbRow) {
         if (colonnePrecedente >= 0) {
-            valeurDeRetour.push(vueTableau[ligneSuivante][colonnePrecedente]);
+            valeurDeRetour.push(laTable.rows[ligneSuivante].cells[colonnePrecedente]);
         }
-        valeurDeRetour.push(vueTableau[ligneSuivante][col]);
+        valeurDeRetour.push(laTable.rows[ligneSuivante].cells[col]);
         if (colonneSuivante < nbCol) {
-            valeurDeRetour.push(vueTableau[ligneSuivante][colonneSuivante]);
+            valeurDeRetour.push(laTable.rows[ligneSuivante].cells[colonneSuivante]);
         }
     }
     return valeurDeRetour;
@@ -127,23 +125,14 @@ var getVoisins = function(row, col) {
  */
 var updateNbVoisins = function() {
     var ts1 = performance.now(),
-        ts2 = null,
-        elems = laTable.getElementsByClassName('estvivante'),
-        i = null,
-        j = null,
-        maCellule = null,
-        maCelluleVoisine = null,
-        tableGetVoisins = null;
-    for (i = 0; i<elems.length; i++) {
-        maCellule = elems[i];
-        tableGetVoisins = getVoisins(parseInt(maCellule.dataset.row), parseInt(maCellule.dataset.col));
-        for (j = 0; j < tableGetVoisins.length; j++) {
-            maCelluleVoisine = tableGetVoisins[j];
+        ts2 = null;
+    [].forEach.call(gameState.aliveCells, function(maCellule) {
+        [].forEach.call(gameState.voisins[maCellule.parentElement.rowIndex][maCellule.cellIndex], function(maCelluleVoisine) {
             // Si la cellule adjacente n'est pas vivante, alors on incrémente son nombre de voisins.
-            // Ce sera utilisé lors du calcul des naissances et.
-            nbVoisins[parseInt(maCelluleVoisine.dataset.row)][parseInt(maCelluleVoisine.dataset.col)]++;
-        }
-    }
+            // Ce sera utilisé lors du calcul des naissances et du reste en vie.
+            gameState.nbVoisins[maCelluleVoisine.parentElement.rowIndex][maCelluleVoisine.cellIndex]++;
+        });
+    });
     ts2 = performance.now();
     console.log('updateNbVoisins: ' + (ts2 - ts1) + 'ms');
 };
@@ -151,46 +140,42 @@ var updateNbVoisins = function() {
 /**
  * Vérification si une cellule reste en vie à la prochaine itération.
  * 
- * @returns {array<jQuery>} Liste des cases vivantes à la prochaine itération
+ * @returns {array<HTMLElement>} Liste des cases vivantes à la prochaine itération
  */
- var getAliveCells = function() {
+ var getAliveCellsNextIteration = function() {
     var ts1 = performance.now(),
         ts2 = null,
-        i = null,
-        j = null,
         tableCellulesAlive = [],
-        ligneEncours = null,
-        celluleEnCours = null,
-        estCelluleVivante = true,
-        lNbVoisins = 0;
-    for (i = 0; i < nbRow; i++) {
-        ligneEncours = vueTableau[i];
-        for (j = 0; j < nbCol; j++) {
-            celluleEnCours = ligneEncours[j];
-            estCelluleVivante = celluleEnCours.classList.contains('estvivante');
-            lNbVoisins = nbVoisins[i][j];
-            // Si la cellule n'est pas vivante, mais qu'elle à 3 voisins, c'est une naissance.
-            // Si la cellule est vivante et qu'elle a deux ou trois voisins, elle reste en vie.
-            if ((!estCelluleVivante && lNbVoisins == 3) || (estCelluleVivante && (lNbVoisins == 2 || lNbVoisins == 3))) {
-                tableCellulesAlive.push(celluleEnCours);
+        maCellule = null;
+    [].forEach.call(gameState.nbVoisins, function(maLigneVoisins, indexLigne) {
+        // On filtre les éléments pour ne garder que les voisins 2 et 3
+        [].forEach.call(maLigneVoisins, function(nbVoisinsCellule, indexColonne) {
+            if (nbVoisinsCellule == 2 || nbVoisinsCellule == 3) {
+                maCellule = laTable.rows[indexLigne].cells[indexColonne];
+                // Si la cellule n'est pas vivante, mais qu'elle à 3 voisins, c'est une naissance.
+                // Si la cellule est vivante et qu'elle a deux ou trois voisins, elle reste en vie.
+                if (maCellule.classList.contains('estvivante') || nbVoisinsCellule == 3) {
+                    tableCellulesAlive.push(maCellule);
+                }
             }
-        }
-    }
+        });
+    });
     ts2 = performance.now();
-    console.log('getAliveCells: ' + (ts2 - ts1) + 'ms');
+    console.log('getAliveCellsNextIteration: ' + (ts2 - ts1) + 'ms');
     return tableCellulesAlive;
 };
 
 /**
  * Mise à jour du DOM avec les élements de la nouvelle itération.
  * 
- * @param {array<jQuery>} TableCellules Tableau contenant les cellules qui seront vivantes à la prochaine itération
+ * @param {array<HTMLElement>} cellulesVivantesIterationSuivantes Tableau contenant les cellules qui seront vivantes à la prochaine itération
  */
-var updateFront = function(TableCellules) {
+var initNextIteration = function(cellulesVivantesIterationSuivantes) {
     var ts1 = performance.now(),
         ts2 = null;
     // tout setup a blanc puis set les bonnes cellules a noir
-    if (TableCellules.length == 0) {
+    if (cellulesVivantesIterationSuivantes.length == 0) {
+        // Plus aucune cellule de vivante. On arrête là...
         document.getElementById('alerteDeFin').innerHTML = '<div class="alert alert-warning alert-dismissible fade show" role="alert"><strong>Fin du jeu&nbsp;!</strong> Plus aucune cellule n\'est vivante. Le jeu ne peut plus continuer...<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Fermer"></button></div>';
         clearInterval(nIntervId);
         nIntervId = null;
@@ -198,18 +183,17 @@ var updateFront = function(TableCellules) {
         [].forEach.call(laTable.querySelectorAll('.estvivante'), function(el) {
             el.classList.remove('estvivante');
         });
-        [].forEach.call(TableCellules, function(el) {
+        [].forEach.call(cellulesVivantesIterationSuivantes, function(el) {
             el.classList.add('estvivante');
         });
-    }
-    // Plus aucune cellule de vivante. On arrête là...
-    if (TableCellules.length == 0) {
-        document.getElementById('alerteDeFin').innerHTML = '<div class="alert alert-warning alert-dismissible fade show" role="alert"><strong>Fin du jeu&nbsp;!</strong> Plus aucune cellule n\'est vivante. Le jeu ne peut plus continuer...<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Fermer"></button></div>';
-        clearInterval(nIntervId);
-        nIntervId = null;
+        gameState.aliveCells = [...cellulesVivantesIterationSuivantes];
+        // On remet à zéro le nombre de voisins
+        [].forEach.call(gameState.nbVoisins, function(el) {
+            el.fill(0);
+        });
     }
     ts2 = performance.now();
-    console.log('updateFront: ' + (ts2 - ts1) + 'ms');
+    console.log('initNextIteration: ' + (ts2 - ts1) + 'ms');
 }
 
 /**
@@ -218,20 +202,16 @@ var updateFront = function(TableCellules) {
 var play = function() {
     var ts1 = performance.now(),
         ts2 = null,
-        tableCellulesVivantesGenerationSuivante = [],
         i = null;
     // Mise à jour du nombre des voisins
     updateNbVoisins();
 
-    // Calcul des cellules vivantes en fonction des voisins.
-    tableCellulesVivantesGenerationSuivante = getAliveCells();
-
     // Mise à jour de l'IHM
-    updateFront(tableCellulesVivantesGenerationSuivante);
+    initNextIteration(getAliveCellsNextIteration());
 
     // Reset du nombre des voisins
     for (i = 0; i < nbRow; i++) {
-        nbVoisins[i].fill(0);
+        gameState.nbVoisins[i].fill(0);
     }
     nbGeneration++;
     generationContainer.value = nbGeneration;
@@ -246,22 +226,29 @@ document.addEventListener('DOMContentLoaded', function() {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')),
         tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
             return new bootstrap.Tooltip(tooltipTriggerEl)
-        });
+        }),
+        btnStart = document.getElementById('start'),
+        btnPause = document.getElementById('pause'),
+        btnRestart = document.getElementById('restart');
 
     initGame();
 
-    document.getElementById('start').addEventListener('click', function() {
+    btnStart.addEventListener('click', function() {
         if (!nIntervId) {
             nIntervId = setInterval(play, nIntervalTime);
         }
+        btnPause.disabled = false;
+        btnStart.disabled = true;
     });
 
-    document.getElementById('pause').addEventListener('click', function() {
+    btnPause.addEventListener('click', function() {
         clearInterval(nIntervId);
         nIntervId = null;
+        btnStart.disabled = false;
+        btnPause.disabled = true;
     });
 
-    document.getElementById('restart').addEventListener('click', function() {
+    btnRestart.addEventListener('click', function() {
         clearInterval(nIntervId);
         nIntervId = null;
         initGame();
